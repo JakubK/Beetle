a2d22d562fd37a3922cf67caf1c14efc1434288e
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-05-22 17:23:31 +0200

    Include AutoPrefixer to the Project

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function()
 {
-    return gulp.src('src/scss/**/*.scss').pipe(sass({outputStyle: 'compressed'}).on('error',sass.logError))
+    return gulp.src('src/scss/**/*.scss').pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
     .pipe(gulp.dest('src/css'));
 });
 
