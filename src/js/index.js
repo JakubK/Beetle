d07fdb98faf4b344bb66197af64ffaddaa917bcd
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2018-05-25 10:27:50 +0200

    Add smooth scrolling and 'active' class switching for navigation

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,52 @@ for (var i = 0; i < circles.length; i++)
     circles[i].style.strokeDasharray = circumference;
     circles[i].style.strokeDashoffset = circumference - percent / 100 * circumference;
 }
+
+
+var navigationElements = document.querySelectorAll("#navigation > li");
+var j = 0;
+navigationElements.forEach(function()
+{
+    var d = j;
+    navigationElements[d].addEventListener('click',function(e)
+    {
+        e.preventDefault(); //prevent default scroll
+        ScrollToSection(navigationElements[d].childNodes[0]);
+        MapNavigation(d,navigationElements); //assign 'active' class only to the clicked element
+    },false);
+    j++;
+});
+
+function MapNavigation(tIndex, navigationElements)
+{
+    navigationElements.forEach(function(x,navigationElements)
+    {
+        x.classList.remove('active');
+    });
+    navigationElements[tIndex].classList.add('active');
+}
+
+var element;
+
+function ScrollToSection(link)
+{
+    if(link != '')
+    {
+        link = link.getAttribute('href').substring(1);
+
+        if(link == '')
+        {
+            element = document.getElementsByTagName("header")[0];
+        }
+        else
+        {
+            element = document.getElementById(link);            
+        }
+
+        if(element !== null)
+        {
+            element.scrollIntoView({block : 'start',behavior : 'smooth'});
+            element.scrollTop -= 30;            
+        }
+    }
+}
